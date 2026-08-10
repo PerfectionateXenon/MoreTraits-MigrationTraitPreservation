892ffb779e1354bd4729b2f028f459330ee3cce7
geod0t <0x4c57@gmail.com>
2026-08-10 12:47:38 +0100

    Fix GymGoer nil error and invalid API call

--- Contents/mods/More Traits/42.20/media/lua/shared/MoreTraits .lua
+++ Contents/mods/More Traits/42.20/media/lua/shared/MoreTraits .lua
@@ -4140,6 +4140,9 @@ local function GymGoerUpdate(player, playerdata)
     end
 
     local fitness = player:getFitness()
+    if not fitness then
+        return
+    end
     if not playerdata.GymGoerStiffnessList then
         playerdata.GymGoerStiffnessList = {
             fitness:getCurrentExeStiffnessInc("arms"),
@@ -4183,7 +4186,7 @@ local function GymGoerUpdate(player, playerdata)
             if isClient() then
                 local bodyParts = {}
                 for _, partType in ipairs(group.parts) do
-                    table.insert(bodyParts, partType:getIndex())
+                    table.insert(bodyParts, BodyPartType.ToIndex(partType))
                 end
                 sendClientCommand(
                         player,
